66d004b6844387c1033edbfe7c6a687f95de8dbe
Demotion <Tion>
2020-03-03 15:25:06 +0100

    Generator progress

--- Assets/Scripts/Grid/GridGenerator.cs
+++ Assets/Scripts/Grid/GridGenerator.cs
@@ -55,31 +55,36 @@ public class GridGenerator : MonoBehaviour
         SetCamSettings(width, height);
     }
 
-    /*void GenerateMap(GridTemplate)
+    void GenerateMap(GridTemplate template)
     {
-        for (int i = 0; i < height; i++)
+        for (int i = 0; i < template.Heigth; i++)
         {
-            for (int j = 0; j < width; j++)
+            for (int j = 0; j < template.Width; j++)
             {
-                switch (infos[i * width + j])
+                switch(template.datas[i * template.Width + j].type)
                 {
-                    case 0:
-                        break;
-                    case 1:
-                        SpawnTile(0, j, i);
+                    case TilesType.Normal:
+                        SpawnTile(j, i, template.datas[i * template.Width + j].mat);
                         break;
                     default:
                         break;
                 }
+                
             }
         }
-        SetCamSettings(width, height);
-    }*/
+        SetCamSettings(template.Width, template.Heigth);
+    }
 
     void SpawnTile(int ID,int GridX, int GridY)
     {
         GameObject GO = Instantiate(tiles[ID], new Vector3(GridX * 2 + 1, 0, GridY * 2 + 1),Quaternion.identity);
         GO.GetComponent<MeshRenderer>().sharedMaterial = debugMaterials[(GridX * 10 + GridY)%debugMaterials.Length];
+    }
+
+    void SpawnTile(int GridX, int GridY,Material tileMat)
+    {
+        GameObject GO = Instantiate(tiles[0], new Vector3(GridX * 2 + 1, 0, GridY * 2 + 1), Quaternion.identity);
+        GO.GetComponent<MeshRenderer>().sharedMaterial = tileMat;
     }
 
     void SetCamSettings(int width,int height)

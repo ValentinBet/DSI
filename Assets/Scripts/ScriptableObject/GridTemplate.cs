e8779345d1a5d7d180afd73ca2fb87b28de9f9e4
LHouyez <l.houyez@rubika-edu.com>
2020-03-03 14:56:38 +0100

    meta de mort

--- Assets/Scripts/ScriptableObject/GridTemplate.cs
+++ Assets/Scripts/ScriptableObject/GridTemplate.cs
@@ -6,14 +6,16 @@ using UnityEngine;
 //[CreateAssetMenu(fileName = "GridTemplate" , menuName = "Kubz/GridTemplate", order = 0  )]
 public class GridTemplate : ScriptableObject
 {
-    public TileEditorData[] datas; 
+    public TileEditorData[] datas;
+    public int Heigth, Width;
+
 
 }
 
 [System.Serializable]
 public struct TileEditorData
-{ 
-   // public int index;
+{
+    // public int index;
     public TilesType type;
     public Material mat;
 }

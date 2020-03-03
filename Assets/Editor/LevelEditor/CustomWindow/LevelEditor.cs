78a794f8ab8dfae88f75421c2f3fe0d752c63a7b
LHouyez <l.houyez@rubika-edu.com>
2020-03-03 11:38:50 +0100

    Enum TilesTypes , scriptable Object GridTemplate & Struct TileEditorData

---
+++ Assets/Editor/LevelEditor/CustomWindow/LevelEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LevelEditor : EditorWindow
+{
+    [MenuItem("Window/Kubz/LevelEditor")]
+    static void Init()
+    {
+        LevelEditor window = (LevelEditor)EditorWindow.GetWindow(typeof(LevelEditor));
+        window.Show();
+    }
+
+
+    string templateName = "level_X_Template";
+    int width = 0, heigth = 0;
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, position.width / 4f, position.height));
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button("Generate Grid"))
+        {
+            Debug.Log("Grid of dimension" + width + " : " + heigth + " as been generated");
+        }
+        GUILayout.EndArea();
+
+    }
+
+}
---
+++ Assets/Editor/LevelEditor/ScriptableObject/GridTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[CreateAssetMenu(fileName = "GridTemplate" , menuName = "Kubz/GridTemplate", order = 0  )]
+public class GridTemplate : ScriptableObject
+{
+    public TileEditorData[] datas; 
+
+}
+
+[System.Serializable]
+public struct TileEditorData
+{ 
+   // public int index;
+    public TilesType type;
+    public Material mat;
+}
+
+
+

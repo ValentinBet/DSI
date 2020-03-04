9985c84f483d4a648c32d3e73658aaaaa12f8f8f
Betrancourt Valentin <valentin.betrancourt@hotmail.fr>
2020-03-04 13:04:10 +0100

    Proto ScreenShake

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using Cinemachine;
 public class CameraManager : MonoBehaviour
 {
+
+    public float maxScreenShakeIntensity = 10;
+
     [SerializeField]
     private Transform camPivot;
     private CinemachineVirtualCamera thisCam;
+    private bool isScreenShaking = false;
 
     public static CameraManager Instance { get { return _instance; } }
     private static CameraManager _instance;
@@ -32,5 +36,30 @@ public class CameraManager : MonoBehaviour
     public void ChangeCamSize(float newSize)
     {
         thisCam.m_Lens.OrthographicSize = newSize;
+    }
+
+    public void InitShakeScreen(float intensity = 2, float duration = 0.1f)
+    {
+        intensity = Mathf.Clamp(intensity, 0, maxScreenShakeIntensity);
+
+        if (!isScreenShaking)
+        {
+            StartCoroutine(ProcessShake(intensity, duration));
+        }
+    }
+
+    private IEnumerator ProcessShake(float intensity, float duration)
+    {
+        isScreenShaking = true;
+        CamerasShake(intensity);
+        yield return new WaitForSeconds(duration);
+        CamerasShake(0);
+        isScreenShaking = false;
+    }
+
+    private void CamerasShake(float intensity)
+    {
+        thisCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = intensity;
+        thisCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = intensity;        
     }
 }

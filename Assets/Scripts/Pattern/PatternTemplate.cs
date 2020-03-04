7c43ca2a6a53ae80b48761925bc82081644ddf69
LHouyez <l.houyez@rubika-edu.com>
2020-03-04 18:28:07 +0100

    Fix levelEditor , Systeme de Partern en cours

---
+++ Assets/Scripts/Pattern/PatternTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "", menuName = "KUBZ/Action Pattern", order = 100)]
+public class PatternTemplate : ScriptableObject
+{
+    public PatternAction[] actions;
+}
+
+public enum ActionType { Movement, Rotation, Attack }
+
+public enum Rotation { Left, Rigth, Reverse }
+
+//public enum AttackType { Projectile , Zone , Normal}
+
+[System.Serializable]
+public struct PatternAction
+{
+    public float actionDuration;
+    public ActionType actionType;
+    public Rotation rotation;
+    // public AttackType AttackType;
+}
+
+

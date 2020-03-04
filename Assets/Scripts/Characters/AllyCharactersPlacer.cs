c9af5d1535c5572c8f0d63cd3c008d07db85c06e
Betrancourt Valentin <valentin.betrancourt@hotmail.fr>
2020-03-04 11:21:21 +0100

    Proto Spawn Alliés

--- Assets/Scripts/Characters/AllyCharactersPlacer.cs
+++ Assets/Scripts/Characters/AllyCharactersPlacer.cs
@@ -1,34 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AllyCharactersPlacer : MonoBehaviour
 {
+    public UnityAction<bool> PlacingAllyCharacters;
+
     public bool isPlacingAllys = false;
     private int lastAllyPriority = 0;
 
-    public List<GameObject> allyTypeList = new List<GameObject>();
+    public List<GameObject> allyList = new List<GameObject>();
 
     public void InitPlacing()
     {
+        PlacingAllyCharacters?.Invoke(true);
+
         isPlacingAllys = true;
     }
 
     public void StopPlacing()
     {
+        PlacingAllyCharacters?.Invoke(false);
         isPlacingAllys = false;
     }
 
-    public void PlaceAlly(TileProperties tile)
+    public void TryPlaceAlly(TileProperties tile)
     {
         if (isPlacingAllys)
         {
-            GameObject _ally = Instantiate(allyTypeList[0], tile.transform.position + Vector3.up, Quaternion.identity);
-            AllyCharacter _allyChar = _ally.GetComponent<AllyCharacter>();
-            _allyChar.SetOccupiedTile();
-            _allyChar.priority = lastAllyPriority;
-            CharactersManager.Instance.allyCharacter.Add(_allyChar);
-            lastAllyPriority++;
+            if (tile.isAllySpawnable && tile.CharacterCanSpawn())
+            {
+                GameObject _ally = Instantiate(allyList[0], tile.transform.position + Vector3.up, Quaternion.identity);
+                AllyCharacter _allyChar = _ally.GetComponent<AllyCharacter>();
+                _allyChar.SetOccupiedTile();
+                _allyChar.priority = lastAllyPriority;
+                CharactersManager.Instance.allyCharacter.Add(_allyChar);
+                lastAllyPriority++;
+            }
+
         }
     }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private static PlayerControl _instance;
+    public static PlayerControl Instance { get { return _instance; } }
+
     public KeyCode TileClearKey;
     public KeyCode TileSwapKey;
     public KeyCode TileRotateKey;
+
+    private bool isPlacingAllyCharacters = false;
+    private AllyCharactersPlacer allyCharactersPlacer;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        allyCharactersPlacer = CharactersManager.Instance.allyCharactersPlacer;
+    }
+
+    private void OnEnable()
+    {
+        allyCharactersPlacer.PlacingAllyCharacters += SetPlacingAllyCharacters;
+    }
+
+    private void OnDisable()
+    {
+        allyCharactersPlacer.PlacingAllyCharacters -= SetPlacingAllyCharacters;
+    }
+
+    private void SetPlacingAllyCharacters(bool value)
+    {
+        isPlacingAllyCharacters = value;
+    }
 
     void Update()
     {
@@ -14,7 +49,15 @@ public class PlayerControl : MonoBehaviour
         {
             if (GridManager.Instance.GetTileUnderSelector() != null)
             {
-                TilesManager.TilesChangerInstance.TryChangePos(GridManager.Instance.GetTileUnderSelector().gameObject);
+                TileProperties _tile = GridManager.Instance.GetTileUnderSelector();
+
+                if (isPlacingAllyCharacters)
+                {
+                    allyCharactersPlacer.TryPlaceAlly(_tile);
+                    return;
+                }
+
+                TilesManager.TilesChangerInstance.TryChangePos(_tile.gameObject);
             }
         }
 

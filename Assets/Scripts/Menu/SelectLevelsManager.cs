247f607d20591eb298ebf27d1ffffd193c765133
Betrancourt Valentin <valentin.betrancourt@hotmail.fr>
2020-03-05 15:05:57 +0100

    début de prod et ajout de 5 Scènes pour les menus (Levels / Champions / settings / Credits et Main menu)

---
+++ Assets/Scripts/Menu/SelectLevelsManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevelsManager : MonoBehaviour
+{
+    public void UnloadLevelsScene()
+    {
+        SceneManager.UnloadSceneAsync("SelectLevels");
+    }
+}

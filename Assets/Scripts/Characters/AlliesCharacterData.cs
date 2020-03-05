cec758f9ae3f7a4f1a59a7aab6fd251375e47484
Betrancourt Valentin <valentin.betrancourt@hotmail.fr>
2020-03-05 13:41:32 +0100

    Début UI menu

---
+++ Assets/Scripts/Characters/AlliesCharacterData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlliesCharacterData : MonoBehaviour
+{
+    public List<AllyCharacter> alliesList = new List<AllyCharacter>();
+}

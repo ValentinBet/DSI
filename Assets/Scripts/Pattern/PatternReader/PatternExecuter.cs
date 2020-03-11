e2f435bce5828b799dc320bd3944757b64955876
LHouyez <l.houyez@rubika-edu.com>
2020-03-11 17:25:03 +0100

    Trap + degat to base good

--- Assets/Scripts/Pattern/PatternReader/PatternExecuter.cs
+++ Assets/Scripts/Pattern/PatternReader/PatternExecuter.cs
@@ -253,10 +253,6 @@ public class PatternExecuter : MonoBehaviour
                         newTile.ChangeTilesActivationStatut(false);
                         StartCoroutine(GetDamaged(pattern, character, index, depth, true, newTile.damageToDeal));
                         character.InitMovement(newTile);
-
-                        //TilesManager.Instance.ChangeTileMaterial(character.occupiedTile, PatternReader.instance.interactionMat);
-                        //tileColoredDuringPattern.Add(character.occupiedTile);
-
                     }
                     else
                     {
@@ -285,7 +281,7 @@ public class PatternExecuter : MonoBehaviour
                         character.transform.position = newPos;
                         PlayerBase.Instance.DamageBase(1);
                         Debug.Log(PlayerBase.Instance.GetLife());
-                        character.KillCharacter(true);
+                        StartCoroutine(GetDamaged(pattern, character, index, depth, false, 100));
                     }
                     break;
 

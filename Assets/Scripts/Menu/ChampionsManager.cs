30759ee7b518d4e90e03becbca0bc9fb36980a7b
LHouyez <l.houyez@rubika-edu.com>
2020-03-12 20:10:04 +0100

    Champ scene

--- Assets/Scripts/Menu/ChampionsManager.cs
+++ Assets/Scripts/Menu/ChampionsManager.cs
@@ -13,15 +13,18 @@ public class ChampionsManager : MonoBehaviour
 
     [SerializeField] private List<TextMeshProUGUI> nameListText = new List<TextMeshProUGUI>();
 
-    [SerializeField] private Image allySprite;
+    [SerializeField] private Sprite[] allySprite;
+    [SerializeField] private Image allyImage;
+
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private TextMeshProUGUI levelText;
     [SerializeField] private TextMeshProUGUI lifePointsText;
-    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private Text descriptionText;
     [SerializeField] private TextMeshProUGUI yearSurvivedText;
     [SerializeField] private TextMeshProUGUI enemyKilledText;
 
     [SerializeField] private GameObject championTab;
+    [SerializeField] private GameObject TabElements;
     [SerializeField] private GameObject patternLayout;
     [SerializeField] private GameObject amuletsLayout;
 
@@ -54,11 +57,13 @@ public class ChampionsManager : MonoBehaviour
 
         UpdateTab(value);
         championTab.SetActive(true);
+        TabElements.SetActive(false);
     }
 
     public void QuitTabElement()
     {
         championTab.SetActive(false);
+        TabElements.SetActive(true);
     }
 
 
@@ -66,8 +71,11 @@ public class ChampionsManager : MonoBehaviour
     {
         AllyCharacterSave _ac = GameInfoManager.GameData.allies[value];
         // allySprite.sprite = _ac.ObjectTypeMetaData.sprite;
+
+
+        allyImage.sprite = allySprite[(int)_ac.type];
         nameText.text = _ac.name;
-        levelText.text = "LEVEL : " + _ac.level;
+        levelText.text = /*"LEVEL : " + */_ac.level.ToString();
         lifePointsText.text = "LIFE POINTS : " +_ac.life;
         descriptionText.text = "DESCRIPTION : \n" + _ac.allyDescription;
         yearSurvivedText.text = "YEAR SURVIVED : " + _ac.yearSurvived;
